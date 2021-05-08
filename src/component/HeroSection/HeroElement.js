73ac3c62ab77ce90476125d6f21d807c8ab9c8b8
KasunHasanga <kasunhasanga96@gmail.com>
2021-05-09 02:49:12 +0530

    add home page content

--- src/component/HeroSection/HeroElement.js
+++ src/component/HeroSection/HeroElement.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
 
 export const HeroContainer = styled.div`
   background: #0c0c0c;
@@ -8,6 +9,22 @@ export const HeroContainer = styled.div`
   height: 800px;
   position: relative;
   z-index: 1;
+
+  :before{
+    content:"";
+    bottom:0;
+    left:0;
+    top:0;
+    background:linear-gradient(
+        180deg,
+        rgba(0,0,0,0.2) 0%,
+        rgba(0,0,0,0.6) 100%.
+        ),
+      linear-gradient(180deg,rgba(0,0,0,0.2) 0%,transparent 100%);
+      z-index:2;
+
+
+    }
 `;
 
 export const HeroBg = styled.div`
@@ -24,6 +41,62 @@ export const VideoBg = styled.video`
   width: 100%;
   height: 100%;
   -o-object-fit: cover;
-  object-fit:cover;
+  object-fit: cover;
   background: #232a34;
+`;
+
+export const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  text-align: center;
+  padding: 8px 24px;
+  margin-top:80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+`;
+export const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 32;
+  }
+`;
+export const HeroP = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+  }
+`;
+
+export const HeroBtnWrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArroForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+  margin-left: 8px;
+  font-size: 20px;
 `;
--- src/component/HeroSection/index.js
+++ src/component/HeroSection/index.js
@@ -1,13 +1,41 @@
-import React from "react";
-import { HeroBg, HeroContainer, VideoBg } from "./HeroElement";
+import React, { useState } from "react";
+import {
+  HeroBg,
+  HeroContainer,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroBtnWrapper,
+  ArroForward,
+  HeroP,
+  ArrowRight,
+} from "./HeroElement";
 import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElement";
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
+            Get Started {hover ? <ArroForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
